0e4f9e4e9ef943ff384841817af102132c2805eb
YYPPOO <kazjiay@gmail.com>
2018-09-14 14:53:42 +0800

    finish game playing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
-import Stylish from "./js/index.jsx";
+import PuzzleGame from "./js/index.jsx";
 import "./css/main.css"
 
 render((
     <BrowserRouter>
-        <Stylish />
+        <PuzzleGame />
     </BrowserRouter>
 ), document.getElementById("cont"));
--- src/js/index.jsx
+++ src/js/index.jsx
@@ -3,174 +3,212 @@ import ReactDOM from "react-dom";
 import { Route, NavLink, Switch } from 'react-router-dom'
 // import { url } from "inspector";
 
-// import Home from "./components/Home";
-// import About from "./components/About";
-// import User from "./components/User";
-// import Contacts from "./components/Contacts";
-
 // header --------------------------------------------------------
-
 class Header extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            tag:""
-        }
     }
     render() {
         return (
-            <div className="header">
-                <NavLink to="/"><img src="../src/img/logo.png" alt="logo"/></NavLink>
-                <div className="menu1">
-                    <ul>
-                        <li className="menuItem"><NavLink to="/">　女　裝　</NavLink></li>
-                        <li>|</li>
-                        <li className="menuItem"><NavLink to="/">　男　裝　</NavLink></li>
-                        <li>|</li>
-                        <li className="menuItem"><NavLink to="/">　配　件　</NavLink></li>
-                    </ul>
-                </div>
-                <div className="menu2">
-                    <div className="searchBar" id="searchBar">
-                        <input className="search" 
-                            id="search" 
-                            placeholder="women, men..." 
-                            onChange={() => this.handleChange(event)}/>
-                        <input type="submit" className="searchIcon" value=""/>
-                    </div>
-                    <CartIcon cartValue={2}/>
-                    <MemberIcon/>
-                </div>
-                <div className="headBlack"></div>
+            <header>
+
+                <NavLink to="/" className="gameButton">Game</NavLink>
+                <NavLink to="/ranking" className="rankingButton">Ranking</NavLink>
+
                 {/* 我們將 App 元件當做每個元件都會載入的母模版，因此可以透過 children 載入對應 URL 的子元件 */}
                 {this.props.children}
-            </div>
+            </header>
         )
-    }
-    handleChange(e) {
-        console.log(e);
-        // if(e.keyCode == 13) {
-        //     this.setState({tag:e.target.value});
-        // }
     }
 }
 
-const CartIcon = (props) => (
-    <NavLink to="/" className="cartDiv">
-        {props.cartValue}
-    </NavLink>
-)
-
-const MemberIcon = (props) => (
-    <NavLink to="/" className="memberDiv">
-        <img src="../src/img/member.png"/>
-    </NavLink>
-)
-
 // section --------------------------------------------------------
-
-const ProductDetail = (props) => (
-    <div>product</div>
-);
-
-const Page2 = (props) => (
-    <div>page2</div>
-);
-
-class Index extends React.Component {
+class Game extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            page:0,
-            tag:"",
-            productList:[]
-        }
     }
     componentWillMount() {
-        fetch("https://appworks-school-stylish.firebaseapp.com/exe/product/list",{
-            method:"get"})
-            .then(res => res.json())
-            .then(res => this.setState({productList:res.list}));
+
     }
     render() {
-        console.log(this.state.productList);
+        let puzzleArray = this.props.puzzleList.map((item,index)=>(
+            <PuzzleItem 
+                key={index} 
+                position={index} 
+                number={item?item:""}
+                onClick={this.props.onClick}
+                enable={this.props.enable}
+            />
+        ));
+        // console.log(puzzleArray)
+        // for(let i=0;i<this.props.puzzleList.length;i++){
+
+        // }
         return (
             <section>
-                <KeyVisual />
-                <ProductList productList={this.state.productList}/>
+                <input 
+                    placeholder="Enter Name" 
+                    value={this.props.name} 
+                    disabled={this.props.enable} 
+                    onChange={this.props.inputName}
+                    />
+                <button
+                    disabled={this.props.enable || !this.props.name}
+                    onClick={() => this.props.start && this.props.start()}
+                    >Start Game</button>
+                <div>Step Count:{this.props.step}</div>
+                <div className="puzzle">
+                    {puzzleArray}
+                </div>
             </section>
         );
     }
 }
 
-class KeyVisual extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return <div>KeyVisual</div>
-    }
-}
-
-class ProductList extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return <div>ProductList</div>
-    }
-}
-
-// footer ------------------------------------------------------
-
-class Footer extends React.Component {
+// puzzleItem ------------------------------------------------------
+class PuzzleItem extends React.Component {
     constructor(props) {
         super(props);
     }
     render() {
         return (
-            <footer>
-                <div className="item">關於 Stylish</div>
-                <div className="mid"></div>
-                <div className="item">服務條款</div>
-                <div className="mid"></div>
-                <div className="item">隱私政策</div>
-                <div className="mid"></div>
-                <div className="item">聯絡我們</div>
-                <div className="mid"></div>
-                <div className="item">FAQ</div>
-                <li><img src="../src/img/facebook.svg" alt="Facebook"/></li>
-                <li><img src="../src/img/twitter.svg" alt="Twitter"/></li>
-                <li><img src="../src/img/youtube.svg" alt="YouTube"/></li>
-                <div className="item">
-                    © 2018. All rights reserved.
-                </div>
-            </footer>
+            <div 
+                key={this.props.position} 
+                className="puzzleItem" 
+                id={"p"+this.props.number}
+                style={{
+                    left: this.props.position%3+"00px",
+                    top: Math.floor(this.props.position/3)+"00px",
+                    opacity: this.props.enable?1:0.3
+                }} 
+                onClick={() => this.props.enable && this.props.onClick(this.props.position,this.props.number)}
+            >{this.props.number}</div>
+        );
+    }
+}
+
+
+// ranking ------------------------------------------------------
+class Ranking extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+
+        return (
+            <section>     Ranking       </section>
+        );
+    }
+}
+
+
+// application -------------------------------------------------
+class PuzzleGame extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            enable:false,
+            step:0,
+            name:"",
+            puzzleList:[1,2,3,4,5,6,7,8,0],
+            playerList:[
+                {
+                    index:0,
+                    name: "GG",
+                    step: 2
+                },
+                {
+                    index:1,
+                    name: "JJ",
+                    step: 5
+                }
+            ]
+        }
+    }
+    inputName(e){
+        console.log(e.target.value);
+        this.setState({name:e.target.value});
+    }
+    findHole(list){
+        // console.log(list.indexOf(0));
+        this.state.hole = list.indexOf(0);
+    }
+
+    handleClick(position){
+        // console.log(position,number);
+        let hx = (this.state.puzzleList.indexOf(0))%3;
+        let hy = Math.floor(this.state.puzzleList.indexOf(0)/3);
+        let x = position%3;
+        let y = Math.floor(position/3);
+        // console.log(hx,hy);
+        // console.log(x,y);
+        if(Math.abs(hx-x)+Math.abs(hy-y)==1){
+            this.change(position,this.state.puzzleList.indexOf(0));
+        }
+    }
+    change(i,j){
+        let newList = this.state.puzzleList;
+        let t = newList[i];
+        newList[i] = newList[j];
+        newList[j] = t;
+        this.setState({puzzleList:newList,step:(this.state.step+1)});
+        console.log(newList.toString());
+        if(newList.toString() == "1,2,3,4,5,6,7,8,0"){
+            this.endGame();
+        }
+    }
+    endGame(){
+        console.log("KO");
+        this.setState({enable:false});
+    }
+    startGame(){
+        // let startList = [1,2,3,4,5,6,7,8,0];
+        // for(let i=0;i<100;i++){
+        //     let randomP = Math.floor(Math.random()*9);
+        //     let hole = startList.indexOf(0);
+        //     let hx = (hole)%3;
+        //     let hy = Math.floor(hole/3);
+        //     let x = randomP%3;
+        //     let y = Math.floor(randomP/3);
+
+        //     if(Math.abs(hx-x)+Math.abs(hy-y)==1){
+        //         let t = startList[hole];
+        //         startList[hole] = startList[randomP];
+        //         startList[randomP] = t;
+        //     }
+        // }
+        // this.setState({enable:true,step:0,puzzleList:startList});
+        this.setState({enable:true,step:0,puzzleList:[1,2,3,4,5,6,7,0,8]});
+    }
+    render(){
+        return(
+            <section>
+                <Header />
+                <Switch>
+                    <Route 
+                        exact path="/" 
+                        render={props => 
+                            <Game 
+                                puzzleList={this.state.puzzleList} 
+                                step={this.state.step} 
+                                onClick={this.handleClick.bind(this)}
+                                start={this.startGame.bind(this)}
+                                enable={this.state.enable}
+                                name={this.state.name}
+                                inputName={this.inputName.bind(this)}
+                            />
+                        }
+                    />
+                        
+                    <Route path="/ranking" component={Ranking} />
+                </Switch>
+            </section>
         )
     }
 }
 
-// application -------------------------------------------------
-
-const Stylish = () => (
-    <section>
-        <Header />
-        <ul>
-            <li><NavLink to="/">Home</NavLink></li>
-            <li><NavLink to="/product">Product</NavLink></li>
-            <li><NavLink to="/page2">page2</NavLink></li>
-        </ul>
-
-        <Switch>
-            <Route exact path="/" component={Index}/>
-            <Route path="/product" component={ProductDetail} />
-            <Route path="/page2" component={Page2} />
-        </Switch>
-        <Footer />
-    </section>
-);
-
-export default Stylish;
+export default PuzzleGame;
 
 // ReactDOM.render((
 //     <Stylish children={routes}  history={browserHistory}/>
